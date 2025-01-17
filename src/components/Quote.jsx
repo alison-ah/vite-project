ed8aac708a2bb18775e5e77437bf5040b7d991f8
Alison <alison@andersonholland.com>
2025-01-17 11:24:08 -0600

    Revised Javascript in Quote.jsx

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "./Button";
 
 const Quote = () => {
-    const [quote, setQuote] = React.useState("");
-    const [author, setAuthor] = React.useState("");
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const quotesUrl =
+    "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+   
+    const fetchApi = async () => {
+        setLoading(true);
+        setError("");
+        try {
+            const response = await fetch(quotesUrl);
+            const quotes = await response.json();
+            selectRandomQuote(quotes);
+        } catch (error) {
+            setError("There was an error. Please try again later.");
+            console.log("There was an error", error);
+        }
+        setLoading(false);
+      };
+    
+    const selectRandomQuote = function (quotes) {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        const randomQuote = quotes[randomIndex];
+        displayQuote(randomQuote);
+    };
 
-    const fetchApi = () => {
-        fetch("https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js")
-            .then((response) => response.json())
-            .then((data) => {
-                const quotes = eval(data);
-                const randomIndex = Math.floor(Math.random() * quotes.length);
-                const randomQuote = quotes[randomIndex];
-
-                setQuote(randomQuote.text);
-                setAuthor(randomQuote.author);
-            })
-            .catch((error) => {
-                console.error("Error fetching quote:", error);
-            });
+    const displayQuote = function (randomQuote) {
+        setAuthor(randomQuote.author);
+        setQuote(randomQuote.text);
     };
 
     return (
