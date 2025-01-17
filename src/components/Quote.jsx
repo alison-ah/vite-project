0d12f9b1afcbb691439fc49c850eb1f49a11f565
Alison <alison@andersonholland.com>
2025-01-16 19:27:25 -0600

    Changed API to Skillcrush quotes

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -6,11 +6,15 @@ const Quote = () => {
     const [author, setAuthor] = React.useState("");
 
     const fetchApi = () => {
-        fetch("http://api.quotable.io/random")
+        fetch("https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js")
             .then((response) => response.json())
             .then((data) => {
-                setQuote(data.content);
-                setAuthor(data.author);
+                const quotes = eval(data);
+                const randomIndex = Math.floor(Math.random() * quotes.length);
+                const randomQuote = quotes[randomIndex];
+
+                setQuote(randomQuote.text);
+                setAuthor(randomQuote.author);
             })
             .catch((error) => {
                 console.error("Error fetching quote:", error);
